95c432480e4ea6c3e6e68255a91d245de018e786
gh-lim <gunhyuck11@gmail.com>
2019-11-19 11:13:21 +0900

    Implement jwt auth server

--- todo-front/src/views/Login.vue
+++ todo-front/src/views/Login.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <h1>로그인 페이지입니다.</h1>
+    <LoginForm />
   </div>
 </template>
 
 <script>
+import LoginForm from '@/components/LoginForm'
 export default {
-
+  name: 'Login',
+  components: {
+    LoginForm,
+  }
 }
 </script>
-
 <style>
 
 </style>
