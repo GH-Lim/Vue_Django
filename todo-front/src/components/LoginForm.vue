26d4ab48d8fbc81607a934fa98366b8008d78d9a
gh-lim <gunhyuck11@gmail.com>
2019-11-20 13:18:29 +0900

    Finish todo app

--- todo-front/src/components/LoginForm.vue
+++ todo-front/src/components/LoginForm.vue
@@ -54,6 +54,10 @@ export default {
             this.$session.start()
             // 응답 결과를 세션에 저장하겠다.
             this.$session.set('jwt', response.data.token)
+
+            // vuex store 를 등록해서 $store로 접근 가능
+            this.$store.dispatch('login', response.data.token)
+
             this.loading = false
             // vue router 를 통해 특정 페이지로 이동
             router.push('/')
--- todo-front/src/views/Home.vue
+++ todo-front/src/views/Home.vue
@@ -8,7 +8,8 @@
 
 <script>
 import axios from 'axios'
-import jwtDecode from 'jwt-decode' // JWT 의 payload 값을 해석해서 보여주는 library
+// import jwtDecode from 'jwt-decode' // JWT 의 payload 값을 해석해서 보여주는 library
+import { mapGetters } from 'vuex'
 import TodoList from '@/components/TodoList'
 import TodoInput from '@/components/TodoInput'
 import router from '@/router'
@@ -24,15 +25,25 @@ export default {
       todos: [],
     }
   },
+  computed: {
+    ...mapGetters([
+      'isLoggedIn',
+      'options',
+      'userId'
+    ])
+  },
   methods: {
     // 사용자 로그인 유무를 확인하여 로그인 되어있지 않을 시 로그인 페이지로 보내겠다.
     checkLoggedIn() {
-      // 1. 세션을 시작해서 
-      this.$session.start()
+      // // 1. 세션을 시작해서 
+      // this.$session.start()
 
-      // 2. 'jwt' 가 있는지 확인하겠다.
-      if (!this.$session.has('jwt')) {
-        // 없다면 로그인 페이지로 보내겠다.
+      // // 2. 'jwt' 가 있는지 확인하겠다.
+      // if (!this.$session.has('jwt')) {
+      //   // 없다면 로그인 페이지로 보내겠다.
+      //   router.push('/login')
+      // }
+      if (!this.isLoggedIn) {
         router.push('/login')
       }
     },
@@ -40,17 +51,17 @@ export default {
     // 우리가 만든 django API 서버로 todos 를 달라는 요청을 보낸뒤
     // todos data 에 할당하는 함수
     getTodo() {
-      this.$session.start()
-      const token = this.$session.get('jwt')
-      const userId = jwtDecode(token).user_id
+      // this.$session.start()
+      // const token = this.$session.get('jwt')
+      // const userId = jwtDecode(token).user_id
       // console.log(decoded.user_id)
       const SERVER_IP = process.env.VUE_APP_SERVER_IP
-      const options = {
-        headers: {
-          Authorization: `JWT ${token}`,
-        }
-      }
-      axios.get(`${SERVER_IP}/api/v1/users/${userId}/`, options)
+      // const options = {
+      //   headers: {
+      //     Authorization: `JWT ${token}`,
+      //   }
+      // }
+      axios.get(`${SERVER_IP}/api/v1/users/${this.userId}/`, this.options)
         .then(response => {
           this.todos = response.data.todo_set
         })
@@ -59,21 +70,21 @@ export default {
         })
     },
     createTodo(title) {
-      this.$session.start()
-      const token = this.$session.get('jwt')
+      // this.$session.start()
+      // const token = this.$session.get('jwt')
       const SERVER_IP = process.env.VUE_APP_SERVER_IP
 
-      const options = {
-        headers: {
-          Authorization: 'JWT ' + token
-        }
-      }
-      const userId = jwtDecode(token).user_id
+      // const options = {
+      //   headers: {
+      //     Authorization: 'JWT ' + token
+      //   }
+      // }
+      // const userId = jwtDecode(token).user_id
       const data = {
         title,
-        user: userId
+        user: this.userId
       }
-      axios.post(`${SERVER_IP}/api/v1/todos/`, data, options)
+      axios.post(`${SERVER_IP}/api/v1/todos/`, data, this.options)
         .then(response => {
           this.todos.push(response.data)
         })
@@ -84,9 +95,17 @@ export default {
   },
   // Vue 가 화면에 그려지면 실행하는 함수
   mounted() {
-    this.checkLoggedIn()
-    this.getTodo()
+    // this.checkLoggedIn()
+    if (this.isLoggedIn) {
+      this.getTodo()
+    }
   },
+  watch: {
+    isLoggedIn() {
+      this.checkLoggedIn()
+      this.getTodo()
+    },
+  }
 }
 </script>
 
